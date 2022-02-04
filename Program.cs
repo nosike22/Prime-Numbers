b1b8bb87800873a7eb8ed83119c56d1ead20c97a
nosike22 <frank.nosike@gmail.com>
2022-02-04 14:57:08 +0100

    Prime Numbers

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Prime_Number_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+            int divisors = 0;
+
+            for (int i = 1; i <= number; i++) 
+            {
+                if (number % 1 == 0) 
+                {
+                    divisors++;
+                }
+
+                if (divisors == 2)
+                {
+                    Console.WriteLine("The Above Number is a prime number");
+
+                }
+
+                else
+                {
+                    Console.WriteLine("The Above Number is not a prime number");
+                    Console.ReadLine();
+                }
+
+                
+           
+            }
+
+
+
+
+        }
+    }
+}
